94ca18567971e3f8a24f6368a06219511eebb58d
Pablo Ermida <pablo.ermida@gmail.com>
2015-06-23 21:00:10 -0300

    Fase 2 - Criando os primeiros models

---
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CodeCommerce\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use CodeCommerce\Http\Requests;
+use CodeCommerce\Http\Controllers\Controller;
+use CodeCommerce\Category;
+
+class AdminCategoriesController extends Controller
+{
+    private $categories;
+
+    public function __construct(Category $category)
+    {
+        $this->categories = $category;
+    }
+
+    public function index()
+    {
+        $categories = $this->categories->all();
+        return view('category', compact('categories'));
+    }
+
+
+}
--- resources/views/exemplo.blade.php
+++
@@ -1,2 +0,0 @@
-<h1>Olá!</h1>
-Meu nome é {{ $nome }} {{ $sobrenome }}
