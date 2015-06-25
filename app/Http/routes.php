7e20b4c301984770ad679b9883941a7dbcfa839e
Pablo Ermida <pablo.ermida@gmail.com>
2015-06-25 14:15:17 -0300

    Correção 2 Fase 3

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,22 +2,24 @@
 
 Route::pattern('id', '[0-9]+');
 
-Route::group(['prefix'=>'admin/categories'], function() {
-    Route::get('/', 'AdminCategoriesController@index');
-    Route::post('/', 'AdminCategoriesController@store');
-    Route::get('create', 'AdminCategoriesController@create');
-    Route::get('show/{id}', 'AdminCategoriesController@show');
-    Route::get('update/{id}', 'AdminCategoriesController@update');
-    Route::get('destroy/{id}', 'AdminCategoriesController@destroy');
-});
+Route::group(['prefix'=>'admin'], function() {
+    Route::group(['prefix'=>'categories'], function() {
+        Route::get('/', 'AdminCategoriesController@index');
+        Route::post('/', 'AdminCategoriesController@store');
+        Route::get('create', 'AdminCategoriesController@create');
+        Route::get('show/{id}', 'AdminCategoriesController@show');
+        Route::get('update/{id}', 'AdminCategoriesController@update');
+        Route::get('destroy/{id}', 'AdminCategoriesController@destroy');
+    });
 
-Route::group(['prefix'=>'admin/products'], function() {
-    Route::get('/', 'AdminProductsController@index');
-    Route::post('/', 'AdminProductsController@store');
-    Route::get('create', 'AdminProductsController@create');
-    Route::get('show/{id}', 'AdminProductsController@show');
-    Route::get('update/{id}', 'AdminProductsController@update');
-    Route::get('destroy/{id}', 'AdminProductsController@destroy');
+    Route::group(['prefix'=>'products'], function() {
+        Route::get('/', 'AdminProductsController@index');
+        Route::post('/', 'AdminProductsController@store');
+        Route::get('create', 'AdminProductsController@create');
+        Route::get('show/{id}', 'AdminProductsController@show');
+        Route::get('update/{id}', 'AdminProductsController@update');
+        Route::get('destroy/{id}', 'AdminProductsController@destroy');
+    });
 });
 
 
