b01df00f7bd825d70168f6bfd02f8a991ed6999c
Pablo Ermida <pablo.ermida@gmail.com>
2015-07-06 19:06:46 -0300

    Comment

--- app/Tag.php
+++ app/Tag.php
@@ -16,8 +16,6 @@ class Tag extends Model
     public function saveTags($tags)
     {
         $tags = array_unique(array_map(function ($str) {
-            //For every tag in the array
-            //Remove white spaces and set to lowercase with first char upper
             return ucwords(strtolower(preg_replace('/\s+/', ' ', trim($str))));
         },
             explode(',', $tags)));
